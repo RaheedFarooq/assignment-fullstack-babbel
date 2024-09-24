21c859a065d4e3a5be8c0f0dc29ecbc634aad014
Raheed Farooq <raheedfarooq65@gmail.com>
2024-09-24 02:32:07 +0200

    fix (server): better route + added cors support

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import cors from 'cors';
 import { getUserEmail, saveNewEmail } from './controller';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 
-app.route('/')
+app.route('/');
+
+app.route('/email')
   .get(getUserEmail)
   .post(saveNewEmail);
 
